8dae83286001ed0df46d25b124db52f7dbe7b9de
benessilfie <bquansah007@gmail.com>
2022-12-15 21:56:40 +0000

    Add solution: Maximum product subarray

--- ruby/maximum_product_subarray.rb
+++ ruby/maximum_product_subarray.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 def max_product(nums)
-    min = max = 1
-    best = nums[0]
+  min = max = 1
+  best = nums[0]
 
-    nums.each do |n|
-        min, max = [n, min*n, max*n].minmax
-        best = [best, max].max
-    end
+  nums.each do |n|
+    min, max = [n, min * n, max * n].minmax
+    best = [best, max].max
+  end
 
-    best
+  best
 end
