d0f61a35a98cfcce42a24758c637398d2309af54
benessilfie <bquansah007@gmail.com>
2022-12-15 21:56:13 +0000

    Add solution: Product except self

--- ruby/product_of_array_except_self.rb
+++ ruby/product_of_array_except_self.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 # @param {Integer[]} nums
 # @return {Integer[]}
 
 def product_except_self(nums)
-    res = [1] * nums.size
+  res = [1] * nums.size
 
-    prefix = 1
-    for i in 0..nums.length-1
-        res[i] *= prefix
-        prefix *= nums[i]
-    end
-    
-    postfix = 1
-    for i in (0..nums.length-1).to_a.reverse
-        res[i] *= postfix
-        postfix *= nums[i]
-    end
-    
-    return res
+  prefix = 1
+  nums.reverse.each_with_index do |_, i|
+    res[i] *= prefix
+    prefix *= nums[i]
+  end
+
+  postfix = 1
+  for i in (0..nums.length-1).to_a.reverse
+    res[i] *= postfix
+    postfix *= nums[i]
+  end
+
+  res
 end
