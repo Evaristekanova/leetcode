c23c0d003691260ee903f6816dd6510ef03e8b8d
benessilfie <bquansah007@gmail.com>
2023-01-30 23:02:33 +0000

    Add solution: Max Profit

--- ruby/max_profit.rb
+++ ruby/max_profit.rb
@@ -1,15 +1,15 @@
 def max_profit(prices)
-maxProfit = 0
-initial_index = 0
+  max_profit = 0
+  initial_index = 0
 
-prices.each do |price|
-  if prices[initial_index] < price
-    profit = price - prices[initial_index]
-    maxProfit = [MaxProfit, profit].max
-  else
-    initial_index = prices.index(price)
+  prices.each do |price|
+    if prices[initial_index] < price
+      profit = price - prices[initial_index]
+      max_profit = [max_profit, profit].max
+    else
+      initial_index = prices.index(price)
+    end
   end
 
-  maxProfit
-    
+  max_profit
 end
