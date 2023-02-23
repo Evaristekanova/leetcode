4f111ed8e55a27818b094fe16926ebd7ef799918
benessilfie <bquansah007@gmail.com>
2023-02-23 23:23:22 +0000

    Add solution: Longest Substring Without Repeating Characters

---
+++ typescript/lengthOfLongestSubstring.ts
@@ -0,0 +1,20 @@
+function lengthOfLongestSubstring(s: string): number {
+
+  const uniqueChars = new Set<string>();
+  let maxLength = 0;
+  let left = 0;
+
+  for (let right = 0; right < s.length; right++) {
+
+    while (uniqueChars.has(s[right])) {
+      uniqueChars.delete(s[left]);
+      left++;
+    }
+
+    uniqueChars.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
