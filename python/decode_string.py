f4473d262f823eb19d15dea9f260376803f0de5c
benessilfie <bquansah007@gmail.com>
2023-03-20 23:00:28 +0000

    Add solution: Decode String

---
+++ python/decode_string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if c == ']':
+                sub = ''
+                while stack[-1] != '[':
+                    sub = stack.pop() + sub
+                stack.pop()
+                num = ''
+                while stack and stack[-1].isdigit():
+                    num = stack.pop() + num
+                stack.append(sub * int(num))
+            else:
+                stack.append(c)
+        return ''.join(stack)
