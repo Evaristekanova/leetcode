5e2946b1b926ba6a64d8e764b181c372a9d217ab
Essilfie <bquansah007@gmail.com>
2023-06-17 23:37:14 +0000

    Add solution: Assign Cookies

---
+++ typescript/findContentChildren.ts
@@ -0,0 +1,18 @@
+function findContentChildren(g: number[], s: number[]): number {
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+
+  let i = 0;
+  let j = 0;
+  let contentChildren = 0;
+
+  while (i < g.length && j < s.length) {
+    if (s[j] >= g[i]) {
+      contentChildren++;
+      i++;
+    }
+    j++;
+  }
+
+  return contentChildren;
+}
